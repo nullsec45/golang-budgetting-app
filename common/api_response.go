e61d9110c327e01b5490293cb73a725cb61e982a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-10 10:40:47 +0700

    feat(common) : add api response.

---
+++ common/api_response.go
@@ -0,0 +1,42 @@
+package common
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type ValidationError struct {
+	Error     string  `json:"error"`
+	Key       string  `json:"key"`
+	Condition string  `json:"condition"`
+}
+
+type JSONErrorResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+type JSONFailedValidationResponse struct {
+	Success bool               `json:"success"`
+	Message string             `json:"message"`
+	Errors  []*ValidationError `json:"errors"`
+}
+
+func SendFailedValidationResponse(c echo.Context, errors []*ValidationError) error {
+	return c.JSON(http.StatusUnprocessableEntity, JSONFailedValidationResponse{
+		Success:false,
+		Errors:errors,
+		Message:"Validation failed",
+	})
+}
+
+func SendErrorResponse(c echo.Context, message string, statusCode int) error {
+	return c.JSON(statusCode, JSONErrorResponse{
+		Success:false,
+		Message:message,
+	})
+}
+
+func SendBadRequestResponse(c echo.Context, message string) error {
+	return SendErrorResponse(c, message, http.StatusBadRequest)
+}
