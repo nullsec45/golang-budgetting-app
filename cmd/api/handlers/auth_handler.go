ffc441605c5e3c9c865d7f42617a0745ae88097a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-10 10:41:08 +0700

    feat(auth) : add valiation in register request.

--- cmd/api/handlers/auth_handler.go
+++ cmd/api/handlers/auth_handler.go
@@ -4,18 +4,29 @@ import (
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"budgetting-app/cmd/api/requests"
-	"fmt"
+	// "github.com/go-playground/validator/v10"
+	"budgetting-app/common"
 )
 
 func (h *Handler) RegisterHandler(c echo.Context) error {
 	payload := new(requests.RegisterUserRequest)	
 
 	if err := (&echo.DefaultBinder{}).BindBody(c, &payload); err != nil {
-		return c.String(http.StatusBadRequest, "bad request")
+		return common.SendBadRequestResponse(c, err.Error())
 	}
-		fmt.Println(payload)
+
+	validationErrors := h.ValidateBodyRequest(c, *payload)
+
+	if validationErrors != nil {
+		return common.SendFailedValidationResponse(c, validationErrors)
+	}
 
 	c.Logger().Info(payload)
+	// var validate *validator.Validate
+	// validate = validator.New(validator.WithRequiredStructEnabled())
+	// err := validate.Struct(payload)
+	// validationErrors := err.(validator.ValidationErrors)
+	// c.Logger().Print(validationErrors)
 
-	return c.String(http.StatusBadRequest, "good request")
+	return c.String(http.StatusOK, "good request")
 }
--- cmd/api/requests/auth_request.go
+++ cmd/api/requests/auth_request.go
@@ -1,8 +1,8 @@
 package requests
 
 type RegisterUserRequest struct {
-	FirstName  string `json:"first_name"`
-	LastName   string `json:"last_name"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
+	FirstName  string `json:"first_name" validate:"required"`
+	LastName   string `json:"last_name" validate:"required"`
+	Email      string `json:"email" validate:"required"`
+	Password   string `json:"password" validate:"required"`
 }
